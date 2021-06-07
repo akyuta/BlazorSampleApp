dc12609041c93f8aae4b7fc886dfb0550432e36d
akyuta <100yen.silver.coin@gmail.com>
2021-06-08 08:48:46 +0900

    add：WeatherForecastアプリケーション追加

---
+++ BlazorSampleApp.Application/WeatherForecast/IWeatherForecastApplication.cs
@@ -0,0 +1,12 @@
+﻿using BlazorSampleApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorSampleApp.Application
+{
+    public interface IWeatherForecastApplication
+    {
+        public Task<List<WeatherForecast>> GetForecastAsync(DateTime startDate);
+    }
+}
