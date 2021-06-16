d8ca7800ef93a4e14daf556dc15d117a02cbd2b6
akyuta <100yen.silver.coin@gmail.com>
2021-06-17 00:54:26 +0900

    add：温度の単位(セルシウス、ファーレンハイト)の値オブジェクト追加

---
+++ BlazorSampleApp.Domain/Temperature/Celsius.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BlazorSampleApp.Domain
+{
+    public class Celsius
+    {
+        public double Value { get; }
+
+        private const double AbsoluteZero = -273.15;
+
+        public Celsius(double value)
+        {
+            if (value < AbsoluteZero) throw new ArgumentOutOfRangeException();
+
+            Value = value;
+        }
+    }
+}
